0f77d6790f876d79a319895fd543ec3923e8cd0f
Menghao Zhang <menghao.zhang@tink.se>
2020-04-22 16:57:13 +0200

    public init for access token

--- Sources/TinkCore/Model/TinkLink/AccessToken.swift
+++ Sources/TinkCore/Model/TinkLink/AccessToken.swift
@@ -6,7 +6,7 @@ public struct AccessToken: Hashable, RawRepresentable, Decodable {
         self.rawValue = rawValue
     }
 
-    init(_ value: String) {
+    public init(_ value: String) {
         self.rawValue = value
     }
 }
