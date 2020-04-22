134e4733494c8f9d830d09315dbbc021bbc7f6ee
Menghao Zhang <menghao.zhang@tink.se>
2020-04-22 17:36:16 +0200

    Use market instead

--- Sources/TinkCore/Model/TinkLink/Mappers/UserProfile+RESTUserProfile.swift
+++ Sources/TinkCore/Model/TinkLink/Mappers/UserProfile+RESTUserProfile.swift
@@ -4,7 +4,7 @@ extension UserProfile {
     init(restUserProfile: RESTUserProfile) {
 //        self.currency = .init(restUserProfile.currency)
         self.locale = .init(identifier: restUserProfile.locale)
-        self.market = .init(restUserProfile.market)
+        self.market = .init(code: restUserProfile.market)
 //        self.periodAdjustedDay = restUserProfile.periodAdjustedDay
 //        self.periodMode = PeriodMode(restPeriodMode: restUserProfile.periodMode)
         self.timeZone = TimeZone(identifier: restUserProfile.timeZone)
--- Sources/TinkCore/Model/UserProfile.swift
+++ Sources/TinkCore/Model/UserProfile.swift
@@ -3,7 +3,7 @@ import Foundation
 public struct UserProfile {
 //    let currency: CurrencyCode
     let locale: Locale
-    let market: CountryCode
+    let market: Market
 //    let periodAdjustedDay: Int
 //    let periodMode: PeriodMode
     let timeZone: TimeZone?
