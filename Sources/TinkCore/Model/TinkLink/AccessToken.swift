2d245d9974fef350c353c1c87cb306af3c9769de
Kasper Lahti <kasper.lahti@tink.se>
2020-06-16 10:41:30 +0200

    Update access token and authorization code

--- Sources/TinkCore/Model/TinkLink/AccessToken.swift
+++ Sources/TinkCore/Model/TinkLink/AccessToken.swift
@@ -1,12 +1,12 @@
-/// An oauth access token to access the Tink service
-public struct AccessToken: Hashable, RawRepresentable, Decodable {
+/// An OAuth access token to access the Tink service.
+public struct AccessToken: Hashable, RawRepresentable {
     public let rawValue: String
 
     public init?(rawValue: String) {
         self.rawValue = rawValue
     }
 
-    public init(_ value: String) {
+    init(_ value: String) {
         self.rawValue = value
     }
 }
--- Sources/TinkCore/Model/TinkLink/AuthorizationCode.swift
+++ Sources/TinkCore/Model/TinkLink/AuthorizationCode.swift
@@ -1,12 +1,18 @@
 /// An authorization code from the Tink backend.
-public struct AuthorizationCode: Hashable, RawRepresentable, Decodable {
+public struct AuthorizationCode: Hashable, RawRepresentable, ExpressibleByStringLiteral {
     public let rawValue: String
 
     public init?(rawValue: String) {
         self.rawValue = rawValue
     }
 
-    init(_ value: String) {
+    public init(stringLiteral value: String) {
+        self.rawValue = value
+    }
+
+    /// Creates an authorization code.
+    /// - Parameter value: The authorization code
+    public init(_ value: String) {
         self.rawValue = value
     }
 }
