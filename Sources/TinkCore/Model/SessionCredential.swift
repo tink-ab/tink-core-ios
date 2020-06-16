07f2311f086ae62ebd2a38c082da1fab5b5d12a4
Menghao Zhang <menghao.zhang@tink.se>
2020-06-16 12:48:31 +0200

    Update the user session form TinkLink

--- Sources/TinkCore/Model/SessionCredential.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-/// A credential to use with the Tink object.
-public enum SessionCredential {
-    /// A session token.
-    case sessionID(String)
-    /// An OAuth access token. 
-    case accessToken(String)
-}
