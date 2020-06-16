5f5118352c4494bc99228bc1a5eec6a8aadd74f6
Kasper Lahti <kasper.lahti@tink.se>
2020-06-16 12:24:11 +0200

    Make signable operation public

--- Sources/TinkCore/Model/SignableOperation.swift
+++ Sources/TinkCore/Model/SignableOperation.swift
@@ -1,21 +1,21 @@
 import Foundation
 
 /// SignableOperation object with the status of the transfer.
-struct SignableOperation {
+public struct SignableOperation {
     /// A unique identifier of a `SignableOperation`.
-    struct ID: Hashable, ExpressibleByStringLiteral {
-        init(stringLiteral value: String) {
+    public struct ID: Hashable, ExpressibleByStringLiteral {
+        public init(stringLiteral value: String) {
             self.value = value
         }
 
-        init(_ value: String) {
+        public init(_ value: String) {
             self.value = value
         }
 
-        let value: String
+        public let value: String
     }
 
-    enum Status {
+    public enum Status {
         case awaitingCredentials
         case awaitingThirdPartyAppAuthentication
         case created
@@ -26,26 +26,26 @@ struct SignableOperation {
         case unknown
     }
 
-    enum Kind {
+    public enum Kind {
         case transfer
     }
 
     /// The timestamp of the creation of the operation.
-    let created: Date?
+    public let created: Date?
     /// The ID of the Credentials used to make the operation.
-    let credentialsID: Credentials.ID?
+    public let credentialsID: Credentials.ID?
     /// The unique identifier of this operation.
-    let id: ID?
+    public let id: ID?
     /// The transfer status. The value of this field changes during payment initiation according to `/resources/payments/payment-status-transitions`
-    let status: Status
+    public let status: Status
     /// A message with additional information regarding the current status of the transfer.
-    let statusMessage: String?
+    public let statusMessage: String?
     /// The type of operation.
-    let kind: Kind
+    public let kind: Kind
     /// The ID of the actual transfer.
-    let transferID: Transfer.ID?
+    public let transferID: Transfer.ID?
     /// The timestamp of the last update of the operation.
-    let updated: Date?
+    public let updated: Date?
     /// The ID of the user making the operation.
     let userID: User.ID?
 }
