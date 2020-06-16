d1b0605c1521bc1ca69267d6c50c92257ce9e6e4
Kasper Lahti <kasper.lahti@tink.se>
2020-06-16 12:25:33 +0200

    Make transfer service public

--- Sources/TinkCore/REST/Services/TinkLink/RESTTransferService.swift
+++ Sources/TinkCore/REST/Services/TinkLink/RESTTransferService.swift
@@ -1,13 +1,17 @@
 import Foundation
 
-final class RESTTransferService: TransferService {
+public final class RESTTransferService: TransferService {
     private let client: RESTClient
+
+    public init(tink: Tink) {
+        self.client = tink.client
+    }
 
     init(client: RESTClient) {
         self.client = client
     }
 
-    func accounts(destinationUris: [URL], completion: @escaping (Result<[Account], Error>) -> Void) -> RetryCancellable? {
+    public func accounts(destinationUris: [URL], completion: @escaping (Result<[Account], Error>) -> Void) -> RetryCancellable? {
         let parameters: [URLQueryItem] = destinationUris.map { URLQueryItem(name: "destination[]", value: $0.absoluteString) }
 
         let request = RESTResourceRequest<RESTAccountListResponse>(path: "/api/v1/transfer/accounts", method: .get, contentType: .json, parameters: parameters) { result in
@@ -18,7 +22,7 @@ final class RESTTransferService: TransferService {
         return client.performRequest(request)
     }
 
-    func transfer(transfer: Transfer, redirectURI: URL, completion: @escaping (Result<SignableOperation, Error>) -> Void) -> RetryCancellable? {
+    public func transfer(transfer: Transfer, redirectURI: URL, completion: @escaping (Result<SignableOperation, Error>) -> Void) -> RetryCancellable? {
         let body = RESTTransferRequest(
             amount: NSDecimalNumber(decimal: transfer.amount).doubleValue,
             credentialsId: transfer.credentialsID?.value,
@@ -40,7 +44,7 @@ final class RESTTransferService: TransferService {
         return client.performRequest(request)
     }
 
-    func transferStatus(transferID: Transfer.ID, completion: @escaping (Result<SignableOperation, Error>) -> Void) -> RetryCancellable? {
+    public func transferStatus(transferID: Transfer.ID, completion: @escaping (Result<SignableOperation, Error>) -> Void) -> RetryCancellable? {
         let request = RESTResourceRequest<RESTSignableOperation>(path: "/api/v1/transfer/\(transferID.value)/status", method: .get, contentType: .json) { result in
             let mappedResult = result.map { SignableOperation($0) }
             completion(mappedResult)
--- Sources/TinkCore/Services/TransferService.swift
+++ Sources/TinkCore/Services/TransferService.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-protocol TransferService {
+public protocol TransferService {
     func accounts(destinationUris: [URL], completion: @escaping (Result<[Account], Error>) -> Void) -> RetryCancellable?
     func transfer(transfer: Transfer, redirectURI: URL, completion: @escaping (Result<SignableOperation, Error>) -> Void) -> RetryCancellable?
     func transferStatus(transferID: Transfer.ID, completion: @escaping (Result<SignableOperation, Error>) -> Void) -> RetryCancellable?
