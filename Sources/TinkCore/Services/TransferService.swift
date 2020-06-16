f964185246ed31b06c3434f7a9b593213c949403
Kasper Lahti <kasper.lahti@tink.se>
2020-06-16 15:47:43 +0200

    Pass separate parameters instead of transfer value

--- Sources/TinkCore/Services/TransferService.swift
+++ Sources/TinkCore/Services/TransferService.swift
@@ -3,7 +3,15 @@ import Foundation
 public protocol TransferService {
     func accounts(destinationUris: [URL], completion: @escaping (Result<[Account], Error>) -> Void) -> RetryCancellable?
     func transfer(
-        transfer: Transfer,
+        amount: Decimal,
+        currency: CurrencyCode,
+        credentialsID: Credentials.ID?,
+        transferID: Transfer.ID?,
+        sourceURI: String,
+        destinationURI: String,
+        sourceMessage: String?,
+        destinationMessage: String,
+        dueDate: Date?,
         redirectURI: URL,
         completion: @escaping (Result<SignableOperation, Error>
     ) -> Void) -> RetryCancellable?
