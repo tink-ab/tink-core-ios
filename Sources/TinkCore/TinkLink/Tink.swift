57bd6e8cb17f4bc6fc96f6b4f7f818ab9cfda856
Menghao Zhang <menghao.zhang@tink.se>
2020-06-16 13:57:06 +0200

    remove the redirect and register for the TinkLinkUI header from the core

--- Sources/TinkCore/TinkLink/Tink.swift
+++ Sources/TinkCore/TinkLink/Tink.swift
@@ -96,36 +96,6 @@ public class Tink {
 
     /// The current configuration.
     public let configuration: Configuration
-
-    // MARK: - Handling Redirects
-
-    ///
-    /// For some providers the redirect needs to be a https link. Use the continue user activity method in your `UIApplicationDelegate` to let Tink send the information to Tink if needed.
-    ///
-    /// ```swift
-    /// func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-    ///     if userActivity.activityType == NSUserActivityTypeBrowsingWeb, let url = userActivity.webpageURL {
-    ///         return Tink.shared.open(url)
-    ///     } else {
-    ///         return false
-    ///     }
-    /// }
-    /// ```
-    @available(iOS 9.0, *)
-    public func open(_ url: URL, completion: ((Result<Void, Error>) -> Void)? = nil) -> Bool {
-        guard let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false),
-            urlComponents.string?.starts(with: configuration.redirectURI.absoluteString) ?? false
-            else { return false }
-
-        var parameters = Dictionary(grouping: urlComponents.queryItems ?? [], by: { $0.name })
-            .compactMapValues { $0.first?.value }
-
-        parameters.merge(urlComponents.fragmentParameters, uniquingKeysWith: { current, _ in current })
-
-        NotificationCenter.default.post(name: .credentialThirdPartyCallback, object: nil, userInfo: parameters)
-
-        return true
-    }
 }
 
 extension Tink {
@@ -181,18 +151,6 @@ extension Tink {
                 completion(.failure(error))
             }
         }
-    }
-}
-
-extension Tink {
-    /// :nodoc:
-    public func _beginUITask() {
-        uiTaskCount += 1
-    }
-
-    /// :nodoc:
-    public func _endUITask() {
-        uiTaskCount -= 1
     }
 }
 
