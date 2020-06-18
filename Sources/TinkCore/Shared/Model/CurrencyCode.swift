5b4ea0130bd88f94a8cbb2c53dfb90bb381bacdc
Kasper Lahti <kasper.lahti@tink.se>
2020-06-18 12:31:15 +0200

    Remove codable conformance from currency denominated amount

--- Sources/TinkCore/Shared/Model/CurrencyCode.swift
+++ Sources/TinkCore/Shared/Model/CurrencyCode.swift
@@ -1,4 +1,4 @@
-public struct CurrencyCode: Hashable, ExpressibleByStringLiteral, Codable {
+public struct CurrencyCode: Hashable, ExpressibleByStringLiteral {
     public let value: String
 
     public init(_ value: String) {
@@ -7,15 +7,5 @@ public struct CurrencyCode: Hashable, ExpressibleByStringLiteral, Codable {
 
     public init(stringLiteral value: String) {
         self.value = value
-    }
-
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        self.value = try container.decode(String.self)
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        try container.encode(value)
     }
 }
