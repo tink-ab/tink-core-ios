89bc44d6a402c6008e83315571f6ba861fb9c5bb
Porraryd <pontus.orraryd@tink.com>
2020-06-23 12:03:23 +0200

    Add PFM services to ServiceContainer

--- Sources/TinkCore/Shared/Tink/ServiceContainer.swift
+++ Sources/TinkCore/Shared/Tink/ServiceContainer.swift
@@ -12,4 +12,12 @@ public final class ServiceContainer {
     public private(set) lazy var providerService: ProviderService = RESTProviderService(client: client)
     public private(set) lazy var transferService: TransferService = RESTTransferService(client: client)
     public private(set) lazy var userService: UserService = RESTUserService(client: client)
+
+    // PFM
+    public private(set) lazy var accountService: AccountService = RESTAccountService(client: client)
+    public private(set) lazy var actionableInsightService: ActionableInsightService = RESTActionableInsightService(client: client)
+    public private(set) lazy var budgetService: BudgetService = RESTBudgetService(client: client)
+    public private(set) lazy var calendarService: CalendarService = RESTCalendarService(client: client)
+    public private(set) lazy var statisticService: StatisticService = RESTStatisticService(client: client)
+    public private(set) lazy var transactionService: TransactionService = RESTTransactionService(client: client)
 }
