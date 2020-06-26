00d005b387c891bf28e63f25ed74f29b62bdfa6e
Porraryd <pontus.orraryd@tink.com>
2020-06-26 15:04:32 +0200

    Make Statistic mapping work properly

--- Sources/TinkCore/PFM/Services/StatisticService.swift
+++ Sources/TinkCore/PFM/Services/StatisticService.swift
@@ -3,7 +3,7 @@ import Foundation
 public protocol StatisticService {
     func statistics(
         description: String?,
-        periods: [DateComponents],
+        periods: [StatisticPeriod],
         types: [Statistic.Kind],
         resolution: Statistic.Resolution,
         padResultsUntilToday: Bool,
