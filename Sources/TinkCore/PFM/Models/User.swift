8a885da471b245343602039e5f27394b9b5211a2
Porraryd <pontus.orraryd@tink.com>
2020-06-29 13:57:55 +0200

    Make properties on User model immutable

--- Sources/TinkCore/PFM/Models/User.swift
+++ Sources/TinkCore/PFM/Models/User.swift
@@ -3,9 +3,9 @@ import Foundation
 public struct User {
     public typealias ID = Identifier<User>
 
-    public var created: Date
-    public var id: ID
-    public var username: String?
-    var nationalID: String?
-    public var profile: UserProfile
+    public let created: Date
+    public let id: ID
+    public let username: String?
+    let nationalID: String?
+    public let profile: UserProfile
 }
