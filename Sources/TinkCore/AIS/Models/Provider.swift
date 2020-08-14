35a984285cb2faaf77bd4175a8f663bbd08c6e6d
Kasper Lahti <kasper.lahti@tink.se>
2020-08-14 11:43:40 +0200

    Remove comparable conformance on AccessType

--- Sources/TinkCore/AIS/Models/Provider.swift
+++ Sources/TinkCore/AIS/Models/Provider.swift
@@ -165,18 +165,7 @@ public struct Provider: Identifiable {
     public let capabilities: Capabilities
 
     /// What Tink uses to access data.
-    public enum AccessType: CustomStringConvertible, Hashable, Comparable {
-        public static func < (lhs: AccessType, rhs: AccessType) -> Bool {
-            switch (lhs, rhs) {
-            case (.openBanking, _):
-                return true
-            case (_, .unknown):
-                return true
-            default:
-                return false
-            }
-        }
-
+    public enum AccessType: CustomStringConvertible, Hashable {
         case unknown
         case openBanking
         case other
