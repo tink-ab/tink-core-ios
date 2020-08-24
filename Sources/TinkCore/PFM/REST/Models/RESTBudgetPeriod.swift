a8b84e175ff0cf829d94dbb320d0325094222ba4
Menghao Zhang <menghao.zhang@tink.se>
2020-08-24 11:42:53 +0200

    Added the budget period and summary

---
+++ Sources/TinkCore/PFM/REST/Models/RESTBudgetPeriod.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct RESTBudgetPeriod: Decodable {
+    /** Period start expressed as UTC epoch timestamp in milliseconds. */
+    let start: Date?
+    /** Period end expressed as UTC epoch timestamp in milliseconds. */
+    let end: Date?
+    /** Period spent amount. */
+    let spentAmount: RESTCurrencyDenominatedAmount?
+}
