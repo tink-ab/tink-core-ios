170e017b76f56c7420c8766ac5abb59df25435db
Menghao Zhang <menghao.zhang@tink.se>
2020-08-24 14:24:31 +0200

    Updated the protocol to return Budget.Transaction

--- Sources/TinkCore/PFM/Services/BudgetService.swift
+++ Sources/TinkCore/PFM/Services/BudgetService.swift
@@ -7,7 +7,7 @@ public protocol BudgetService {
 
     func editBudget(id: Budget.ID, request: UpdateBudgetRequest, completion: @escaping (Result<Budget, Error>) -> Void) -> Cancellable?
 
-    func budgetTransactionByID(_ id: Budget.ID, start: Date, end: Date, completion: @escaping (Result<[Transaction], Error>) -> Void) -> Cancellable?
+    func budgetTransactionByID(_ id: Budget.ID, start: Date, end: Date, completion: @escaping (Result<[Budget.Transaction], Error>) -> Void) -> Cancellable?
 
     func budgetSummaries(includeArchived: Bool, completion: @escaping (Result<[BudgetSummary], Error>) -> Void) -> Cancellable?
 
