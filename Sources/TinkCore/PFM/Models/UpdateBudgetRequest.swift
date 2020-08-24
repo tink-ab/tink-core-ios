2fa152cafaafaa4a5bfc098c840ed864d3f66b8a
Menghao Zhang <menghao.zhang@tink.se>
2020-08-24 13:55:46 +0200

    Public model

--- Sources/TinkCore/PFM/Models/UpdateBudgetRequest.swift
+++ Sources/TinkCore/PFM/Models/UpdateBudgetRequest.swift
@@ -1,12 +1,12 @@
 import Foundation
 
-struct UpdateBudgetRequest {
+public struct UpdateBudgetRequest {
     /** The name of the Budget. */
-    var name: String
+    public var name: String
     /** The target amount for the budget. The currency must match the user profile currency setting. */
-    var amount: CurrencyDenominatedAmount
+    public var amount: CurrencyDenominatedAmount
     /** The filter defining the budget and which transactions that is included in it. The configured fields of the filter are applied as logical and operator (intersection). */
-    var filter: [Budget.Filter]
+    public var filter: [Budget.Filter]
     /** Periodicity configuration for the budget. */
-    var periodicity: Budget.Periodicity
+    public var periodicity: Budget.Periodicity
 }
