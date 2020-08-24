8fd4862c2eb57682d9706dac5e3165ef7cab4449
Menghao Zhang <menghao.zhang@tink.se>
2020-08-24 13:56:00 +0200

    Updated the protocol to be public

--- Sources/TinkCore/PFM/Services/BudgetService.swift
+++ Sources/TinkCore/PFM/Services/BudgetService.swift
@@ -1,17 +1,17 @@
+import Foundation
+
 public protocol BudgetService {
     func budgets(includeArchived: Bool, completion: @escaping (Result<[Budget], Error>) -> Void) -> Cancellable?
 
-    func createBudget(request: UpdateBudgetRequest, completion: @escaping (Result<RESTUpdateBudgetResponse, Error>) -> Void) -> Cancellable?
+    func createBudget(request: UpdateBudgetRequest, completion: @escaping (Result<Budget, Error>) -> Void) -> Cancellable?
 
-    func editBudget(id: Budget.ID, request: UpdateBudgetRequest, completion: @escaping (Result<RESTUpdateBudgetResponse, Error>) -> Void) -> Cancellable?
+    func editBudget(id: Budget.ID, request: UpdateBudgetRequest, completion: @escaping (Result<Budget, Error>) -> Void) -> Cancellable?
 
-    func budgetTransactionByID(_ id: Budget.ID, start: Date, end: Date, completion: @escaping (Result<RESTBudgetTransactionsResponse, Error>) -> Void) -> Cancellable?
+    func budgetTransactionByID(_ id: Budget.ID, start: Date, end: Date, completion: @escaping (Result<[Transaction], Error>) -> Void) -> Cancellable?
 
-    func budgets(includeArchived: Bool = false, completion: @escaping (Result<RESTListBudgetSpecificationsResponse, Error>) -> Void) -> Cancellable?
+    func budgetSummaries(includeArchived: Bool, completion: @escaping (Result<[BudgetSummary], Error>) -> Void) -> Cancellable?
 
-    func budgetSummaries(includeArchived: Bool = false, completion: @escaping (Result<RESTListBudgetSummariesResponse, Error>) -> Void) -> Cancellable?
+    func budgetDetailsByID(_ budgetID: Budget.ID, start: Date, end: Date, completion: @escaping (Result<BudgetPeriodOverview, Error>) -> Void) -> Cancellable?
 
-    func budgetDetailsByID(_ budgetID: Budget.ID, start: Date, end: Date, completion: @escaping (Result<RESTBudgetDetailsResponse, Error>) -> Void) -> Cancellable?
-
-    func archiveBudget(_ budgetID: Budget.ID, completion: @escaping (Result<RESTArchiveBudgetResponse, Error>) -> Void) -> Cancellable?
+    func archiveBudget(_ budgetID: Budget.ID, completion: @escaping (Result<Budget, Error>) -> Void) -> Cancellable?
 }
