ec03ee0280725b4d1ed383aff5f3479506a76f4f
Menghao Zhang <menghao.zhang@tink.se>
2020-08-24 12:06:41 +0200

    Added the budget period and period overview

---
+++ Sources/TinkCore/PFM/Models/BudgetPeriod.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+public struct BudgetPeriod: Equatable {
+    /** Period start expressed as UTC epoch timestamp in milliseconds. */
+    public var start: Date
+    /** Period end expressed as UTC epoch timestamp in milliseconds. */
+    public var end: Date
+    /** Period spent amount. */
+    public var spentAmount: CurrencyDenominatedAmount?
+}
---
+++ Sources/TinkCore/PFM/Models/BudgetPeriodOverview.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public struct BudgetPeriodOverview {
+    /** The budget. */
+    public var budgetSpecification: Budget
+    /** List of budget periods. */
+    public var budgetPeriods: [BudgetPeriod]
+    /** First period start expressed as UTC epoch timestamp in milliseconds. */
+    public var start: Date
+    /** Last period end expressed as UTC epoch timestamp in milliseconds. */
+    public var end: Date
+    /** Total amount spent within the listed periods. */
+    public var totalSpentAmount: CurrencyDenominatedAmount?
+    /** Average period spending for the listed periods. */
+    public var averageSpentAmount: CurrencyDenominatedAmount?
+}
+
