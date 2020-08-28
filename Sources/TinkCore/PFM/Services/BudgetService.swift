4926c2c7c8687993e9142d96fb586bec0a341506
Menghao Zhang <menghao.zhang@tink.se>
2020-08-28 10:43:16 +0200

    Updated the budget service protocol

--- Sources/TinkCore/PFM/Services/BudgetService.swift
+++ Sources/TinkCore/PFM/Services/BudgetService.swift
@@ -3,15 +3,15 @@ import Foundation
 public protocol BudgetService {
     func budgets(includeArchived: Bool, completion: @escaping (Result<[Budget], Error>) -> Void) -> Cancellable?
 
-    func createBudget(request: UpdateBudgetRequest, completion: @escaping (Result<Budget, Error>) -> Void) -> Cancellable?
+    func create(name: String, amount: CurrencyDenominatedAmount, filter: [Budget.Filter], periodicity: Budget.Periodicity, completion: @escaping (Result<Budget, Error>) -> Void) -> Cancellable?
 
-    func editBudget(id: Budget.ID, request: UpdateBudgetRequest, completion: @escaping (Result<Budget, Error>) -> Void) -> Cancellable?
+    func update(id: Budget.ID, name: String, amount: CurrencyDenominatedAmount, filter: [Budget.Filter], periodicity: Budget.Periodicity, completion: @escaping (Result<Budget, Error>) -> Void) -> Cancellable?
 
-    func budgetTransactionByID(_ id: Budget.ID, start: Date, end: Date, completion: @escaping (Result<[Budget.Transaction], Error>) -> Void) -> Cancellable?
+    func transactionsForBudget(id: Budget.ID, start: Date, end: Date, completion: @escaping (Result<[Budget.Transaction], Error>) -> Void) -> Cancellable?
 
     func budgetSummaries(includeArchived: Bool, completion: @escaping (Result<[BudgetSummary], Error>) -> Void) -> Cancellable?
 
-    func budgetDetailsByID(_ budgetID: Budget.ID, start: Date, end: Date, completion: @escaping (Result<BudgetPeriodOverview, Error>) -> Void) -> Cancellable?
+    func budgetDetails(id: Budget.ID, start: Date, end: Date, completion: @escaping (Result<BudgetPeriodOverview, Error>) -> Void) -> Cancellable?
 
-    func archiveBudget(_ budgetID: Budget.ID, completion: @escaping (Result<Budget, Error>) -> Void) -> Cancellable?
+    func archive(id: Budget.ID, completion: @escaping (Result<Budget, Error>) -> Void) -> Cancellable?
 }
