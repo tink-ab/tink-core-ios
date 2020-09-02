c8744bc228b1e6882c5e23b9ff5fc8e3aadc351c
Porraryd <pontus.orraryd@tink.com>
2020-09-02 17:12:31 +0200

    Add public initializer to User

--- Sources/TinkCore/PFM/Models/User.swift
+++ Sources/TinkCore/PFM/Models/User.swift
@@ -8,4 +8,12 @@ public struct User {
     public let username: String?
     let nationalID: String?
     public let profile: UserProfile
+
+    public init(created: Date, id: User.ID, username: String?, nationalID: String?, profile: UserProfile) {
+        self.created = created
+        self.id = id
+        self.username = username
+        self.nationalID = nationalID
+        self.profile = profile
+    }
 }
