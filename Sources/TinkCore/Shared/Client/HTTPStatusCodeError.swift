72be3d54f3789647f64fc9406b7509250dfa7712
Kasper Lahti <kasper.lahti@tink.se>
2020-09-11 14:08:03 +0200

    Remove duplicate status

--- Sources/TinkCore/Shared/Client/HTTPStatusCodeError.swift
+++ Sources/TinkCore/Shared/Client/HTTPStatusCodeError.swift
@@ -37,7 +37,6 @@ public enum HTTPStatusCodeError: Error {
         case 401: self = .unauthorized
         case 403: self = .forbidden
         case 404: self = .notFound
-        case 404: self = .notFound
         case 409: self = .conflict
         case 412: self = .preconditionFailed
         case 451: self = .unavailableForLegalReasons
