f7f22c07de14cac8257083acb70cbb85272ea9f1
Porraryd <pontus.orraryd@tink.com>
2020-09-23 15:17:47 +0200

    Update release script

--- Scripts/core_release.sh
+++ Scripts/core_release.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
-git checkout master
-git pull
-git checkout -b release-candidate
-
-rm -rf ./build
-rm -rf ./TinkCore.xcframework
-
 echo Enter new pod spec version:
 read newVersion
 
+git checkout master
+git pull
+git checkout -b rc-$newVersion
+
 sed -i "" "s/[0-9]\.[0-9]\.[0-9]/$newVersion/g" TinkCore.podspec
 sed -i "" "s/[0-9]\.[0-9]\.[0-9]/$newVersion/g" project.yml
+
 git commit -am"Update version"
 
 make format
 git commit -am"Format project"
 
+rm -rf ./build
+rm -rf ./TinkCore.xcframework
+
 make cartage-project
 make framework
 
 mv ./build/TinkCore.xcframework ./
-git commit -am"Update framework"
+
+git add .
+git commit -m"Update framework"
 
 echo Done! Push this your private fork of Tink Core
