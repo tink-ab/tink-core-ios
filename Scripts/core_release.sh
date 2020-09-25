a08fe12b4f54a34871a48851e4cd2befe5eadeeb
Mani Sedighi <mani.sedighi@tink.com>
2020-09-25 11:41:11 +0200

    Check version format before continue

--- Scripts/core_release.sh
+++ Scripts/core_release.sh
@@ -2,9 +2,14 @@
 echo Enter new pod spec version:
 read newVersion
 
+if [[ $newVersion =~ ^([0-9]{1,2}\.){2}[0-9]{1,10}$ ]]; then
 git checkout master
 git pull
 git checkout -b rc-$newVersion
+else
+  echo "$newVersion is not in the right format."
+  exit
+fi
 
 sed -i "" 's/  spec.version      = "[0-9]\.[0-9]\.[0-9]"/  spec.version      = "'$newVersion'"/' TinkCore.podspec
 sed -i "" 's/      MARKETING_VERSION: [0-9]\.[0-9]\.[0-9]/      MARKETING_VERSION: '$newVersion'/' project.yml
