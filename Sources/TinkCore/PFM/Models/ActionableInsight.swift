e518cb865dc924c7278baafb7d275369f252f1ef
Kasper Lahti <kasper.lahti@tink.se>
2020-09-25 16:44:32 +0200

    Add typealias to not break api

--- Sources/TinkCore/PFM/Models/ActionableInsight.swift
+++ Sources/TinkCore/PFM/Models/ActionableInsight.swift
@@ -294,6 +294,9 @@ public extension ActionableInsight {
     }
 
     struct SuggestSetUpSavingsAccount {
+        @available(*, deprecated, message: "Use ActionableInsight.AccountInfo instead.")
+        typealias AccountInfo = ActionableInsight.AccountInfo
+
         public let balance: CurrencyDenominatedAmount
         public let savingsAccount: AccountInfo
         public let currentAccount: AccountInfo
