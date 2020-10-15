817b0cd64341e07c781b1d35f48a804c84698de2
Porraryd <pontus.orraryd@tink.com>
2020-10-15 17:03:28 +0200

    Add documentation to transaction properties

--- Sources/TinkCore/PFM/Models/Transaction.swift
+++ Sources/TinkCore/PFM/Models/Transaction.swift
@@ -5,14 +5,21 @@ public struct Transaction {
     /// A unique identifier of a `Transaction`.
     public typealias ID = Identifier<Transaction>
 
+    /// The internal identifier of the account that the transaction belongs to.
     public let accountID: Account.ID
+    /// The amount of the transaction.
     public let amount: CurrencyDenominatedAmount
+    /// The category of the transaction.
     public var categoryID: Category.ID
+    /// The description of the transaction.
     public let description: String
+    /// The date the transaction was executed.
     public let date: Date
     /// The unique identifier of this `Transaction`.
     public let id: ID
+    /// The timestamp of when the transaction was first saved to database.
     public let inserted: Date
+    /// Indicates if this is an upcoming transaction not booked yet.
     public let isUpcomingOrInFuture: Bool
 
     public init(id: ID, accountID: Account.ID, amount: CurrencyDenominatedAmount, categoryID: Category.ID, description: String, date: Date, inserted: Date, isUpcomingOrInFuture: Bool) {
