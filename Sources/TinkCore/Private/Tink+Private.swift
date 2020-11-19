12f8e8f1494a854e336324358021cfa216eed6e2
Kasper Lahti <kasper.lahti@tink.se>
2020-11-19 13:10:44 +0100

    Don't set user session in create temporary user call

--- Sources/TinkCore/Private/Tink+Private.swift
+++ Sources/TinkCore/Private/Tink+Private.swift
@@ -2,16 +2,8 @@ import Foundation
 
 extension Tink {
     @discardableResult
-    public func _createTemporaryUser(for market: Market, locale: Locale = Tink.defaultLocale, completion: @escaping (Result<Void, Swift.Error>) -> Void) -> RetryCancellable? {
-        return services.oAuthService.createAnonymous(market: market, locale: locale, origin: nil) { [weak self] result in
-            do {
-                let accessToken = try result.get()
-                self?.userSession = .accessToken(accessToken.rawValue)
-                completion(.success)
-            } catch {
-                completion(.failure(error))
-            }
-        }
+    public func _createTemporaryUser(for market: Market, locale: Locale = Tink.defaultLocale, completion: @escaping (Result<AccessToken, Swift.Error>) -> Void) -> RetryCancellable? {
+        return services.oAuthService.createAnonymous(market: market, locale: locale, origin: nil, completion: completion)
     }
 
     public var _sdkName: String {
