76eedec5173d50dcbe9e2b5a4ed55cf3a5947db2
Kasper Lahti <kasper.lahti@tink.se>
2020-11-19 10:14:10 +0100

    Remove user error

--- Sources/TinkCore/Private/Tink+Private.swift
+++ Sources/TinkCore/Private/Tink+Private.swift
@@ -4,9 +4,8 @@ extension Tink {
     @discardableResult
     public func _createTemporaryUser(for market: Market, locale: Locale = Tink.defaultLocale, completion: @escaping (Result<Void, Swift.Error>) -> Void) -> RetryCancellable? {
         return services.oAuthService.createAnonymous(market: market, locale: locale, origin: nil) { [weak self] result in
-            let mappedResult = result.mapError { UserError(createTemporaryUserError: $0) ?? $0 }
             do {
-                let accessToken = try mappedResult.get()
+                let accessToken = try result.get()
                 self?.userSession = .accessToken(accessToken.rawValue)
                 completion(.success)
             } catch {
