de72140391869bb8776d82e5a7f2519c229c26bc
Kasper Lahti <kasper.lahti@tink.se>
2021-01-20 15:24:39 +0100

    Conform to defaultable decodable protocol

--- Sources/TinkCore/PFM/REST/Models/RESTPeriod.swift
+++ Sources/TinkCore/PFM/REST/Models/RESTPeriod.swift
@@ -1,10 +1,12 @@
 import Foundation
 
 struct RESTPeriod: Decodable {
-    enum Resolution: String, Codable {
+    enum Resolution: String, DefaultableDecodable, Encodable {
         case monthly = "MONTHLY"
         case monthlyAdjusted = "MONTHLY_ADJUSTED"
         case unknown
+
+        static var decodeFallbackValue: Resolution = .unknown
     }
 
     let endDate: Date?
