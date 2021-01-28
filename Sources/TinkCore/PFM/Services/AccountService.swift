934481ab609047eae0dc9ac845a1641fab5c5515
Kasper Lahti <kasper.lahti@tink.se>
2021-01-28 16:53:35 +0100

    Add update method to service protocol

--- Sources/TinkCore/PFM/Services/AccountService.swift
+++ Sources/TinkCore/PFM/Services/AccountService.swift
@@ -1,3 +1,12 @@
 public protocol AccountService {
     func accounts(completion: @escaping (Result<[Account], Error>) -> Void) -> Cancellable?
+    func update(
+        id: Account.ID,
+        name: String,
+        type: Account.Kind,
+        accountNumber: String,
+        isFavorite: Bool,
+        ownership: Double,
+        completion: @escaping (Result<Account, Error>) -> Void
+    ) -> Cancellable?
 }
