3fa759a652a5328ac689d190ce15af0949eff4b9
Mani Sedighi <mani.sedighi@tink.com>
2021-03-02 11:03:02 +0100

    Update SelectOptions and make it public

--- Sources/TinkCore/AIS/Models/SelectOption.swift
+++ Sources/TinkCore/AIS/Models/SelectOption.swift
@@ -2,14 +2,14 @@ import Foundation
 /// A list of options where the user should select one.
 public struct SelectOption {
     /// A URL the client can optionally use to show an icon to represent the option.
-    let iconUrl: String?
+    public let iconURL: URL?
     /// The human-readable description of this option to display to the user.
-    let text: String?
+    public let text: String?
     /// The machine-readable value to send if the user picks this option.
-    let value: String?
+    public let value: String?
 
-    init(iconUrl: String, text: String, value: String) {
-        self.iconUrl = iconUrl
+    init(iconURL: URL, text: String, value: String) {
+        self.iconURL = iconURL
         self.text = text
         self.value = value
     }
