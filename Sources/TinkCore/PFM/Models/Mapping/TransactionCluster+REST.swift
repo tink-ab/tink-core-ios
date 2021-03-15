b6d9b7ebdb60d7e4c81937d2760530b6a5fe7662
Mani Sedighi <31520382+lilSwifty@users.noreply.github.com>
2021-03-15 16:03:16 +0100

    Update Sources/TinkCore/PFM/Models/Mapping/TransactionCluster+REST.swift
    Co-authored-by: Kasper Lahti <kasper.lahti@tink.com>

--- Sources/TinkCore/PFM/Models/Mapping/TransactionCluster+REST.swift
+++ Sources/TinkCore/PFM/Models/Mapping/TransactionCluster+REST.swift
@@ -2,8 +2,8 @@ import Foundation
 
 extension TransactionCluster {
     init(from restTransactionCluster: RESTTransactionCluster) {
-        self.categorizationImprovement = .init(restTransactionCluster.categorizationImprovement)
-        self.description = .init(restTransactionCluster.description)
+        self.categorizationImprovement = restTransactionCluster.categorizationImprovement
+        self.description = restTransactionCluster.description
         self.transactions = restTransactionCluster.transactions.map { Transaction.init(from: $0) } 
     }
 }
