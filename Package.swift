ca6728a4cd07e521d818e4568b59d6c7922d5032
Menghao Zhang <menghao.zhang@tink.se>
2021-04-01 14:28:15 +0200

    Update package file

--- Package.swift
+++ Package.swift
@@ -16,6 +16,9 @@ let package = Package(
         )
     ],
     targets: [
-        .target(name: "TinkCore", exclude: ["Info.plist"]),
+        .binaryTarget(
+            name: "TinkCore",
+            url: "https://github.com/tink-ab/tink-core-ios/releases/download/1.3.0/TinkCore.xcframework.zip", checksum: "d8df585865080a3f1caa7e2c683e115113f186e31a73ed91753305afee4b5cb7"
+        ),
     ]
 )
