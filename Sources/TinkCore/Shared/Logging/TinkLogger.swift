99fc0e524202c1f2e39b62591e61584ba54853bb
tink-ab-automation <tink-ab-automation@tink.com>
2022-08-11 15:30:55 +0200

    Release version 1.8.1-test

---
+++ Sources/TinkCore/Shared/Logging/TinkLogger.swift
@@ -0,0 +1,9 @@
+import OSLog
+
+public struct TinkLogger {
+    public static func logUsedSDK(version: String, subsystem: String, category: String) {
+        let app = OSLog(subsystem: subsystem, category: category)
+        let string = "Version: \(version)"
+        os_log("%@", log: app, type: .info, string)
+    }
+}
