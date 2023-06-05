78552202d16a11797bc595a7d236d049c8ab7689
tink-ab-automation <tink-ab-automation@tink.com>
2023-06-05 11:39:44 +0200

    Release version 1.11.1

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TinkCore",
-            url: "https://github.com/tink-ab/tink-core-ios/releases/download/1.11.0/TinkCore.xcframework.zip", checksum: "a75f6a8e08bff48411cff889635761960245c582da1ad98911039201c0b2305f"
+            url: "https://github.com/tink-ab/tink-core-ios/releases/download/1.11.1/TinkCore.xcframework.zip", checksum: "014b351b4a0263cb05baf93cf332f2b471f940e270fa7e2337fbe5d6502dc20e"
         ),
     ]
 )
