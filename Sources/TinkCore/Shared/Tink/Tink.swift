0835bafa0184135da85e75f5f2ae49b0cc8daaef
runner <runner@Mac-1713360101765.local>
2024-04-17 13:35:26 +0000

    Release version 2.3.0-MON-1619

--- Sources/TinkCore/Shared/Tink/Tink.swift
+++ Sources/TinkCore/Shared/Tink/Tink.swift
@@ -4,7 +4,7 @@ import Foundation
 #endif
 
 /// Current TinkCore version.
-public let version = "2.2.0"
+public let version = "2.3.0"
 
 /// The `Tink` class encapsulates a connection to the Tink API.
 ///
